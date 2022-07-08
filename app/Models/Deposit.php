0efef9513e6940cbee7df032588288049f646977
JoaoV1t0r <joaovitor.silva2pereira@gmail.com>
2022-07-08 19:40:09 -0300

    fix models

--- app/Models/Deposit.php
+++ app/Models/Deposit.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Deposit extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'account_id',
+        'amount',
+    ];
+
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
 }
--- app/Models/Transfer.php
+++ app/Models/Transfer.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use YourAppRocks\EloquentUuid\Traits\HasUuid;
 
 class Transfer extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuid;
+
+    protected $fillable = [
+        'uuid',
+        'origin_account',
+        'destination_account',
+        'amount',
+    ];
+
+    public function originAccount(): BelongsTo
+    {
+        return $this->belongsTo(Account::class, 'origin_account');
+    }
+
+    public function destinationAccount(): BelongsTo
+    {
+        return $this->belongsTo(Account::class, 'destination_account');
+    }
 }
